c2fabd55813e966ef37f9467493002681b699f2a
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2022-08-17 14:37:25 -0500

    *shrug*

--- src/main/java/me/justahuman/bugshowcase/AllayListener.java
+++ src/main/java/me/justahuman/bugshowcase/AllayListener.java
@@ -4,12 +4,19 @@ import org.bukkit.entity.Allay;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class AllayListener implements Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     public void AllayGiveListener(PlayerInteractEntityEvent event) {
+        if (event.getRightClicked() instanceof Allay) event.setCancelled(true);
+        event.getPlayer().sendMessage(event.getRightClicked().getUniqueId() + " ? " + event.getRightClicked().getEntityId());
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void AllayAtListener(PlayerInteractAtEntityEvent event) {
         if (event.getRightClicked() instanceof Allay) event.setCancelled(true);
     }
 }
